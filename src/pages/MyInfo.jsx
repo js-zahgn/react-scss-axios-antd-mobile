9d08ae7a2c57f2d7f84b768b9d2adf6d52e71e68
js-zahgn <156231639@qq.com>
2018-10-15 14:18:10 +0800

    add context API

--- src/pages/MyInfo.jsx
+++ src/pages/MyInfo.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import { Switch,Route,Link } from 'react-router-dom';
 
+import { UserContext,ThemeContext } from '../store/context';
+
+import ContextChild from '../components/mods/ContextChild';
+
 class TheInfo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.changeContextMsg = (e) => {
+      console.log(e.target.value)
+      const { themeContext } = this.state;
+      themeContext.message = e.target.value;
+      this.setState({
+        themeContext
+      });
+    };
+
+    this.state = {
+      userContext: 'this is user',
+      themeContext: {
+        message: 'theme context',
+        fn: this.changeContextMsg
+      }
+    };
+  }
+
   render() {
     return (
     <div>
       <h2>个人信息</h2>
+        <UserContext.Provider value={this.state.userContext}>
+          <ThemeContext.Provider value={this.state.themeContext}>
+            <ContextChild/>
+          </ThemeContext.Provider>
+        </UserContext.Provider>
       <ul>
         <li>
           <Link to="/info/rendering">Rendering with React Routers</Link>
